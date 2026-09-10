f80f92d1addc964aea3e2dac17edaaf8a1f98fea
LakshyaJohari <lakshyajohari2006@gmail.com>
2026-09-10 13:49:49 +0530

    fix: update Google OAuth Client ID to match Google Cloud Console

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "300155415762-25qg5k7imnj4tfn8ojpq7defg006kbpt.apps.googleusercontent.com"}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "300155415762-uo0o71jjg28i313djufo49jk3mfe527m.apps.googleusercontent.com"}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
